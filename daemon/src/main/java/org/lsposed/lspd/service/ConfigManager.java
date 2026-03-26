f915fcf2020c5ef83937856a5fd0f15bbd81d0df
JingMatrix <jingmatrix@gmail.com>
2026-03-26 16:46:27 +0100

    fix compilation

--- daemon/src/main/java/org/lsposed/lspd/service/ConfigManager.java
+++ daemon/src/main/java/org/lsposed/lspd/service/ConfigManager.java
@@ -34,6 +34,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteStatement;
+import android.os.Binder;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
